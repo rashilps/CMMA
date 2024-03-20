c3eb54104f6dc41b6031b99f9bcfb6b2573d9e54
Rashil <rashil1993@gmail.com>
2024-03-20 23:50:50 +0530

    1

--- app/src/main/java/com/example/cmma/MaterialQuantityUpdateUser.java
+++ app/src/main/java/com/example/cmma/MaterialQuantityUpdateUser.java
@@ -10,5 +10,6 @@ public class MaterialQuantityUpdateUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_quantity_update_user);
+
     }
 }
